8e0c0732347c363e4630c8c0cbeff95abc81786e
benkimani447-sys <benkimani447@gmail.com>
2025-11-26 16:19:50 +0300

    Add files via upload

---
+++ Replicating _invoice.java
@@ -0,0 +1,19 @@
+
+public class Main{
+    public static void main(String[]args){
+        System.out.println("Bill To:\t\t\t\t\t\t\t\t\tShip to:\n...IZV\nContact:+91-7869825463\t\t\t\t\t\tContact:+91-7869825463" +
+                "\nEmail:purchase-team@thendral.com\t\t\tEmail:purchase-team@thendral.com");
+        System.out.println("\n\tPayment Date :7 days from date of delivery\t\tPayment Terms :100% against invoice");
+        System.out.println("\nS.No\tProduct Code\tProduct Name\t\tHSN code\t\tQuantity\t\tUnits\t\tRate\t\tTax\t\tAmount");
+        System.out.println("1\t\t105\t\t\t\tSurf Excel 5 kg \t 34019011\t\t 20\t\t\t\tnos\t\t\t 600.00 \t 5% \t 12600.00");
+        System.out.println("2\t\t109\t\t\t\tRin 1 kg\t\t\t 34019011\t\t 25\t\t\t\tnos\t\t\t 85.00\t\t5% \t\t 2231.00");
+        System.out.println("3\t\t115\t\t\t\tHamam Soap 150g\t\t34011100\t\t25\t\t\t\tnos\t\t\t60.00\t\t5%\t\t1575.00");
+        System.out.println("4\t\t120\t\t\t\tLux Soap 150g\t\t34011100\t\t30\t\t\t\tnos\t\t\t53.00\t\t5%\t\t443.00");
+        System.out.println("5\t\t125\t\t\t\tDove soap 125g\t\t34011100\t\t25\t\t\t\tnos\t\t\t75.00\t\t5%\t\t1968.00");
+        System.out.println("6\t\t129\t\t\t\tVim bar 200g\t\t08109010\t\t20\t\t\t\tnos\t\t\t15.00\t\t5%\t\t315.00");
+        System.out.println("7\t\t131\t\t\t\tPepsodent 150g\t\t33061020\t\t30\t\t\t\tnos\t\t\t85.00\t\t18%\t\t3009.00");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tTotal\t\t22141.00");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tDiscounts\t141.00");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tGrand total\t22000.00");
+    }
+}
